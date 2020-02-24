abc9d3b788aa378dc0418c80b2589bd2c2ab9c45
pauloricardodn <32545045+pauloricardodn@users.noreply.github.com>
2020-02-24 10:41:42 -0300

    Criando e configurando projeto

---
+++ DigitalMaps/src/main/java/com/digitalmaps/api/DigitalMapsApiApplication.java
@@ -0,0 +1,13 @@
+package com.digitalmaps.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalMapsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalMapsApiApplication.class, args);
+	}
+
+}
---
+++ DigitalMaps/src/test/java/com/digitalmaps/api/DigitalMapsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalmaps.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalMapsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
